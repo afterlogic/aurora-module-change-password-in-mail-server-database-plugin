86af5fecafa82c8e267d5885660146ec41b6d901
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:16:48 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,7 +8,8 @@
 namespace Aurora\Modules\ChangePasswordInMailServerDatabasePlugin;
 
 /**
- *
+ * Allows users to change passwords on their email accounts, assuming the passwords are stored in a database table.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
