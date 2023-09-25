39c28184e0c1c78b3a91a5c89ae6aafdcc29aa6a
Vasil Sokolov <vasil@afterlogic.com>
2023-09-25 17:38:31 +0200

    Code formatting

--- Module.php
+++ Module.php
@@ -13,7 +13,7 @@ namespace Aurora\Modules\ChangePasswordInMailServerDatabasePlugin;
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2023, Afterlogic Corp.
- * 
+ *
  * @property Settings $oModuleSettings
  *
  * @package Modules
